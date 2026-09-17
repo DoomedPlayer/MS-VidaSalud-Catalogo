ecd380e30d0914443bc219ddbc4cb4fcfe1de97c
DoomedPlayer <ni.beeger@duocuc.cl>
2026-09-17 18:19:14 -0300

    Fix prestacion

--- src/main/java/com/BinarySeint/vsCatalog/classes/Prestacion.java
+++ src/main/java/com/BinarySeint/vsCatalog/classes/Prestacion.java
@@ -10,42 +10,58 @@ import jakarta.persistence.Table;
 @Entity 
 @Table (name = "prestaciones")
 public class Prestacion {
+
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, length = 100)
     private String nombre;
+
     @Column(nullable = false)
     private Double precio;
+
     @Column(length = 255)
     private String descripcion;
 
-
     public Prestacion(){}
 
-    public Prestacion(Long id,String nombre,Double precio,String descripcion){
+    public Prestacion(Long id, String nombre, Double precio, String descripcion){
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
-        this.descripcion=descripcion;
+        this.descripcion = descripcion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNombre(){
         return nombre;
     }
+
     public void setNombre(String nombre){
-        this.nombre =nombre;
+        this.nombre = nombre;
     }
+
     public Double getPrecio(){
         return precio;
     }
+
     public void setPrecio(Double precio){
         this.precio = precio;
     }
+
     public String getDescripcion(){
         return descripcion;
     }
+
     public void setDescripcion(String descripcion){
-        this.descripcion =descripcion;
+        this.descripcion = descripcion;
     }
 }
